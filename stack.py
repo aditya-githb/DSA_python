2b7fae517e052211585670442f96aa09a13c55b3
GODofGOD007 <112172428+GODofGOD007@users.noreply.github.com>
2023-07-24 21:58:32 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,17 @@
+stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print("Initial Stack")
+print(stack)
+
+print("stack elements before pooped ")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print("stack elements after popped")
+
+print(stack)
